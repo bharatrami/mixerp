f27858b5ed8caeb355f2ccd1c68c71544a010f2a
binodnp <binod@planetearthsolution.com>
2013-07-27 01:13:28 +0545

    Routine development.

---
+++ MixERP.net.DatabaseLayer/DBFactory/Sanitizer.cs
@@ -0,0 +1,44 @@
+﻿/********************************************************************************
+    Copyright (C) Binod Nepal, Planet Earth Solutions Pvt. Ltd., Kathmandu.
+	Released under the terms of the GNU General Public License, GPL, 
+	as published by the Free Software Foundation, either version 3 
+	of the License, or (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+    See the License here <http://www.gnu.org/licenses/gpl-3.0.html>.
+***********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MixERP.net.DatabaseLayer.DBFactory
+{
+    public static class Sanitizer
+    {
+        /// <summary>
+        /// Please do not use this function to fix the quotes against SQL injection attack.
+        /// This is not a replacement of parameterized statements.
+        /// Use this function only when you need to sanitize "column names" and/or "table names"
+        /// which cannot be done using standard practices.
+        /// </summary>
+        /// <param name="identifier">Column name or table name which needs to be sanitized</param>
+        /// <returns>
+        /// Only alphabets and underscore are allowed characters in identifier name.
+        /// Anything else than that will be removed.
+        /// </returns>
+        public static string SanitizeIdentifierName(string identifier)
+        {
+            //No comment.
+            if(identifier.Contains("--")){return string.Empty;}
+            if(identifier.Contains("/*")){return string.Empty;}
+
+            //Removing the match else than alphabets and underscore.
+            return Regex.Replace(identifier, @"[^a-zA-Z_]", "");
+        }
+
+    }
+}
