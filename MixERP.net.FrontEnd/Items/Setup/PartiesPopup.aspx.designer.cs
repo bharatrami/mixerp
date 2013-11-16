895b5144df0b4f80b134d06a000d36cd022aadfd
Binod Nirvan <binodnp@outlook.com>
2013-11-16 14:25:32 +0545

    Routine Development.

--- MixERP.net.FrontEnd/Items/Setup/PartiesPopup.aspx.designer.cs
+++ MixERP.net.FrontEnd/Items/Setup/PartiesPopup.aspx.designer.cs
@@ -31,12 +31,12 @@ namespace MixERP.Net.FrontEnd.Items.Setup {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl container;
         
         /// <summary>
-        /// ToolkitScriptManager1 control.
+        /// ScriptManager1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::AjaxControlToolkit.ToolkitScriptManager ToolkitScriptManager1;
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
     }
 }
--- MixERP.net.FrontEnd/Services/PartyData.asmx.cs
+++ MixERP.net.FrontEnd/Services/PartyData.asmx.cs
@@ -1,78 +1,64 @@
-﻿using AjaxControlToolkit;
-using System.Collections.ObjectModel;
-/********************************************************************************
+﻿/********************************************************************************
 Copyright (C) Binod Nepal, Mix Open Foundation (http://mixof.org).
 
 This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. 
 If a copy of the MPL was not distributed  with this file, You can obtain one at 
 http://mozilla.org/MPL/2.0/.
 ***********************************************************************************/
-using System.Collections.Specialized;
-using System.Linq;
-using System.Web.Script.Services;
+using System;
+using System.Web;
 using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Web.Script.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Web.UI.WebControls;
+using System.Collections.ObjectModel;
 
 namespace MixERP.Net.FrontEnd.Services
 {
-    /// <summary>
-    /// Summary description for PartyData
-    /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-    [System.ComponentModel.ToolboxItem(false)]
-    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
     [ScriptService]
     public class PartyData : System.Web.Services.WebService
     {
-
-        [WebMethod(EnableSession = true)]
-        public CascadingDropDownNameValue[] GetParties(string knownCategoryValues, string category)
-        {
-            using(System.Data.DataTable table = MixERP.Net.BusinessLayer.Helpers.FormHelper.GetTable("core", "parties"))
-            {
-                string displayField = MixERP.Net.Common.Helpers.ConfigurationHelper.GetDbParameter( "PartyDisplayField");
-                table.Columns.Add("party", typeof(string), displayField);
-                return this.GetValues(table);
-            }
-
-        }
-
-        private CascadingDropDownNameValue[] GetValues(System.Data.DataTable table)
-        {
-            Collection<CascadingDropDownNameValue> values = new Collection<CascadingDropDownNameValue>();
-
-            foreach(System.Data.DataRow dr in table.Rows)
-            {
-                values.Add(new CascadingDropDownNameValue(dr["party"].ToString(), dr["party_code"].ToString()));
-            }
-
-            return values.ToArray();
-        }
-
         [WebMethod]
-        public CascadingDropDownNameValue[] GetShippingAddresses(string knownCategoryValues, string category)
+        public Collection<ListItem> GetParties()
         {
-            Collection<CascadingDropDownNameValue> values = new Collection<CascadingDropDownNameValue>();
+            Collection<ListItem> values = new Collection<ListItem>();
 
-            StringDictionary kv = CascadingDropDown.ParseKnownCategoryValuesString(knownCategoryValues);
-            string partyCode = kv["Party"];
-
-            using(System.Data.DataTable table = MixERP.Net.BusinessLayer.Core.ShippingAddresses.GetShippingAddressView(partyCode))
+            using (System.Data.DataTable table = MixERP.Net.BusinessLayer.Helpers.FormHelper.GetTable("core", "parties"))
             {
-                string displayField = MixERP.Net.Common.Helpers.ConfigurationHelper.GetDbParameter( "ShippingAddressDisplayField");
-                table.Columns.Add("shipping_address", typeof(string), displayField);
-
-                foreach(System.Data.DataRow dr in table.Rows)
+                string displayField = MixERP.Net.Common.Helpers.ConfigurationHelper.GetDbParameter("PartyDisplayField");
+                table.Columns.Add("party", typeof(string), displayField);
+                foreach (System.Data.DataRow dr in table.Rows)
                 {
-                    values.Add(new CascadingDropDownNameValue(dr["shipping_address_code"].ToString(), dr["shipping_address"].ToString()));
+                    values.Add(new ListItem(dr["party"].ToString(), dr["party_code"].ToString()));
                 }
             }
 
-            return values.ToArray();
-
+            return values;
         }
 
+        [WebMethod]
+        public Collection<ListItem> GetAddressByPartyCode(string partyCode)
+        {
+            Collection<ListItem> values = new Collection<ListItem>();
 
+            using (System.Data.DataTable table = MixERP.Net.BusinessLayer.Core.ShippingAddresses.GetShippingAddressView(partyCode))
+            {
+                string displayField = MixERP.Net.Common.Helpers.ConfigurationHelper.GetDbParameter("ShippingAddressDisplayField");
+                table.Columns.Add("shipping_address", typeof(string), displayField);
+
+                foreach (System.Data.DataRow dr in table.Rows)
+                {
+                    values.Add(new ListItem(dr["shipping_address_code"].ToString(), dr["shipping_address"].ToString()));
+                }
+            }
+
+            return values;
+        }
     }
+
 }
