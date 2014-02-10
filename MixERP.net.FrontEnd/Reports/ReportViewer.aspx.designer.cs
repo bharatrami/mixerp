aaa7edb135359aafd5de0e14ce98bac9dceb76e6
Binod Nepal <nepalbinod@yahoo.com>
2014-02-11 01:20:21 +0545

    Bug fixes on MixERP reports.

--- MixERP.net.FrontEnd/Reports/ReportViewer.aspx.designer.cs
+++ MixERP.net.FrontEnd/Reports/ReportViewer.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace MixERP.Net.FrontEnd.Reports {
     public partial class ReportViewer {
         
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
         /// ReportParameterPanel control.
         /// </summary>
         /// <remarks>
